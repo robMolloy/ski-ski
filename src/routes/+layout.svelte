4549a7c9813dbe98e757a05a5012c7c7f1fec21a
robMolloy <robmolloy@hotmail.co.uk>
2024-02-29 10:16:49 +0000

    sveltekit starter project + tailwind setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+	import '../app.css';
+</script>
+
+<div class="app">
+	<Header />
+	<div class="flex gap-5">
+		<div>asd</div>
+		<div>asd</div>
+		<div>asd</div>
+		<div>asd</div>
+		<div>asd</div>
+		<div>asd</div>
+		<div>asd</div>
+	</div>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
